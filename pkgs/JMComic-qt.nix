fcb66cd4dc1141557395ee146f9e134fc2c6a390
zerozawa <zekaixie37@gmail.com>
2025-10-16 02:04:32 +0800

    fix: JMComic-qt & picacg-qt back to appimage

--- pkgs/JMComic-qt.nix
+++ pkgs/JMComic-qt.nix
@@ -1,85 +1,32 @@
 {
+  fetchurl,
+  appimageTools,
   lib,
-  stdenv,
-  fetchFromGitHub,
-  fuse,
-  xorg,
-  libxcb-util ? null,
-  libxcb ? xorg.libxcb,
-  makeWrapper,
-  vulkan-loader,
-  python3,
-  python3-waifu2x-vulkan,
-  python3-jmcomic,
   ...
 }: let
-  python =
-    python3.withPackages
-    (ps:
-      with ps; [
-        pyinstaller
-        pyside6
-        pillow
-        lxml
-        pycryptodomex
-        pysocks
-        natsort
-        curl-cffi
-        webdavclient3
-        tqdm
-        pysmb
-        beautifulsoup4
-        (httpx.overridePythonAttrs (finalAttrs:
-          with finalAttrs; {
-            dependencies = dependencies ++ (with optional-dependencies; http2 ++ socks);
-          }))
-        python3-jmcomic
-        (python3-waifu2x-vulkan.override
-          {inherit python3;})
-      ]);
+  pname = "JMComic-qt";
+  version = "1.2.9";
+  src = fetchurl {
+    url = "https://github.com/tonquer/${pname}/releases/download/v${version}/jmcomic_v${version}_linux-glibc2.38.AppImage";
+    hash = "sha256-LgHR+HDfTb9Ur8p4Ibb8TUdLqwkK8wKynrKliYbEGSg=";
+  };
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
 in
-  stdenv.mkDerivation rec {
-    pname = "JMComic-qt";
-    version = "1.2.9";
-    src = fetchFromGitHub {
-      owner = "tonquer";
-      repo = pname;
-      rev = "v${version}";
-      hash = "sha256-cvDdA5xewawO7ccs3Zv1xfTW9zHImnu+xLUKvq3Fpns=";
-    };
-    nativeBuildInputs = [
-      fuse
-      makeWrapper
-      python
+  appimageTools.wrapAppImage {
+    inherit pname version;
+    src = appimageContents;
+    extraPkgs = pkgs: [
+      (pkgs.libxcb or pkgs.xorg.libxcb)
+      (pkgs.libxcb-util or pkgs.xorg.xcbutil)
     ];
-    buildInputs =
-      [
-        vulkan-loader
-        libxcb
-      ]
-      ++ (
-        lib.optional (libxcb-util != null) libxcb-util
-      );
-    buildPhase = ''
-      runHook preBuild
-      pyinstaller --hidden-import=_cffi_backend --collect-data curl_cffi --add-data "./lib/linux/*:."  -w src/start.py
-      runHook postBuild
-    '';
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/share
-      cp -r dist/start $out/share/${pname}
-      mkdir -p $out/bin
-      ln -s $out/share/${pname}/start $out/bin/${pname}
+    extraInstallCommands = ''
       mkdir -p $out/share/applications
-      cp $src/res/appimage/*.desktop $out/share/applications/${pname}.desktop
+      cp ${appimageContents}/JMComic.desktop $out/share/applications/
       mkdir -p $out/share/pixmaps
-      cp $src/res/icon/logo_round.png $out/share/pixmaps/${pname}.png
-      wrapProgram $out/bin/${pname} \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath buildInputs}
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace-fail JMComic ${pname}
-      runHook postInstall
+      cp ${appimageContents}/JMComic.png $out/share/pixmaps/
+      substituteInPlace $out/share/applications/JMComic.desktop --replace-fail 'Exec=JMComic' 'Exec=${pname}'
     '';
     meta = with lib; {
       description = "tonquer/JMComic-qt: 禁漫天堂，18comic，使用qt实现的PC客户端，支持Windows，Linux，MacOS";
@@ -87,6 +34,6 @@ in
       platforms = with platforms; (intersectLists x86_64 linux);
       license = with licenses; [lgpl3];
       mainProgram = pname;
-      sourceProvenance = with sourceTypes; [fromSource];
+      sourceProvenance = with sourceTypes; [binaryBytecode];
     };
   }
--- pkgs/picacg-qt.nix
+++ pkgs/picacg-qt.nix
@@ -1,88 +1,32 @@
 {
-  stdenv,
+  fetchurl,
+  appimageTools,
   lib,
-  fetchFromGitHub,
-  python3,
-  python3-waifu2x-vulkan,
-  picacg-database,
-  fuse,
-  makeWrapper,
-  xorg,
-  libxcb-util ? null,
-  libxcb ? xorg.libxcb,
-  vulkan-loader,
   ...
 }: let
   pname = "picacg-qt";
   version = "1.5.2";
-  python =
-    python3.withPackages
-    (ps:
-      with ps; [
-        pyinstaller
-        pyside6
-        websocket-client
-        pillow
-        pysocks
-        natsort
-        webdavclient3
-        tqdm
-        pysmb
-        lxml
-        (httpx.overridePythonAttrs (finalAttrs:
-          with finalAttrs; {
-            dependencies = dependencies ++ (with optional-dependencies; http2 ++ socks);
-          }))
-        (python3-waifu2x-vulkan.override
-          {inherit python3;})
-      ]);
-  runtimeDep =
-    [
-      vulkan-loader
-      libxcb
-    ]
-    ++ (
-      lib.optional (libxcb-util != null) libxcb-util
-    );
+  src = fetchurl {
+    url = "https://github.com/tonquer/${pname}/releases/download/v${version}/bika_v${version}_linux_glibc2.38.AppImage";
+    hash = "sha256-zAok08R9Xdx6z6JBGNoQlxPrSVvxkquYg9fgf8OMj1o=";
+  };
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
 in
-  stdenv.mkDerivation rec {
+  appimageTools.wrapAppImage {
     inherit pname version;
-    src = fetchFromGitHub {
-      owner = "tonquer";
-      repo = pname;
-      rev = "v${version}";
-      hash = "sha256-tsIEfcUsI3RFSmFf2uXgQpbjHOIOqhupgZmRQdtoDoU=";
-    };
-    nativeBuildInputs = [
-      fuse
-      makeWrapper
-      python
+    src = appimageContents;
+    extraPkgs = pkgs: [
+      (pkgs.libxcb or pkgs.xorg.libxcb)
+      (pkgs.libxcb-util or pkgs.xorg.xcbutil)
     ];
-    buildInputs = runtimeDep ++ [picacg-database];
-    buildPhase = ''
-      runHook preBuild
-      pyinstaller -w src/start.py
-      runHook postBuild
-    '';
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/share
-      cp -r dist/start $out/share/${pname}
-      mkdir -p $out/bin
-      ln -s $out/share/${pname}/start $out/bin/${pname}
+    extraInstallCommands = ''
       mkdir -p $out/share/applications
-      cp $src/res/appimage/*.desktop $out/share/applications/${pname}.desktop
+      cp ${appimageContents}/PicACG.desktop $out/share/applications/
       mkdir -p $out/share/pixmaps
-      cp $src/res/icon/logo_round.png $out/share/pixmaps/${pname}.png
-      wrapProgram $out/bin/${pname} \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath runtimeDep} \
-        --run 'if [ ! -d ~/.picacg/db ];then mkdir -p ~/.picacg/db; echo "mkdir db";fi' \
-        --run 'if [ ! -d ~/.picacg/data ];then mkdir -p ~/.picacg/data; echo "mkdir data";fi' \
-        --run 'if [ ! -f ~/.picacg/version ];then touch ~/.picacg/db/version; echo "mkdir version";fi' \
-        --run 'if [ ! -f ~/.picacg/db/book.db ] || [ "`cat ~/.picacg/db/version`" != "${picacg-database.version}" ] ; then cp -f ${picacg-database}/share/${picacg-database.pname}/book.db ~/.picacg/db/;echo "${picacg-database.version}" > ~/.picacg/db/version;echo "copy db";fi'
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace-fail PicACG ${pname}
-      runHook postInstall
+      cp ${appimageContents}/PicACG.png $out/share/pixmaps/
+      substituteInPlace $out/share/applications/PicACG.desktop --replace-fail 'Exec=PicACG' 'Exec=${pname}'
     '';
     meta = with lib; {
       description = "tonquer/picacg-qt: 哔咔漫画, PicACG comic PC client(Windows, Linux, MacOS)";
@@ -90,6 +34,6 @@ in
       platforms = with platforms; (intersectLists x86_64 linux);
       license = with licenses; [lgpl3];
       mainProgram = pname;
-      sourceProvenance = with sourceTypes; [fromSource];
+      sourceProvenance = with sourceTypes; [binaryBytecode];
     };
   }
